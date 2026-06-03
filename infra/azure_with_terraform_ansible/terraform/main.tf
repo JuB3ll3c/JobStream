bf0d60dc8f58402aa5aff78b2cab7f459e0fd33f
Julien Bellec <bellec.julien@pm.me>
2026-06-03 16:17:28 +0200

    Add terraform script to create infra on azure Add ansible scripts to provision infra Add github workflows to build images and deploy infra + app

---
+++ infra/azure_with_terraform_ansible/terraform/main.tf
@@ -0,0 +1,179 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-ansible-free-lab"
+  location = "westeurope"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-lab"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-ansible-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# ==========================================
+# # APP VM: HAS A PUBLIC IP FOR SSH CONNECTION AND ACTS AS A JUMPBOX TO ACCESS THE PRIVATE KAFKA VM
+# ==========================================
+
+resource "azurerm_public_ip" "pip_app" {
+  name                = "pip-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic_app" {
+  name                = "nic-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.2.10"
+    public_ip_address_id          = azurerm_public_ip.pip_app.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc_app" {
+  network_interface_id      = azurerm_network_interface.nic_app.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm_app" {
+  name                = "app-server"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ts_v2" # Ta VM de 2 vCPUs
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic_app.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# ==========================================
+# VM FOR KAFKA BROKER
+# ==========================================
+
+resource "azurerm_network_interface" "nic_kafka" {
+  name                = "nic-kafka"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.2.20"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc_kafka" {
+  network_interface_id      = azurerm_network_interface.nic_kafka.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm_kafka" {
+  name                = "kafka-server"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ts_v2" # Ta VM de 2 vCPUs
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic_kafka.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# ==========================================
+# GENERATE ANSIBLE INVENTORY
+# ==========================================
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/ansible/inventory/hosts.yml"
+
+  content  = <<EOT
+all:
+  vars:
+    ansible_connection: ssh
+    ansible_user: azureuser
+    ansible_ssh_private_key_file: ~/.ssh/id_rsa
+    ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
+  children:
+    app_servers:
+      hosts:
+        ${azurerm_public_ip.pip_app.ip_address}:
+
+    private_infrastructure:
+      vars:
+        ansible_ssh_common_args: '-o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p -q azureuser@${azurerm_public_ip.pip_app.ip_address}"'
+      children:
+        kafka_brokers:
+          hosts:
+            ${azurerm_network_interface.nic_kafka.ip_configuration[0].private_ip_address}:
+EOT
+}
