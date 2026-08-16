200850cfeadf7c392b5437eaf1db521ae0b3db8c
JuB3ll3c <bellec.julien@protonmail.com>
2026-08-17 00:57:38 +0200

    FEAT: add proxy.conf.json + add context-path for the backend

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BASE_PATH } from './generated/variables';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: BASE_PATH, useValue: '/api' }
   ]
 };
